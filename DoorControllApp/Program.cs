80e4a62d1877f080f832b09d1241fc3c4454a962
Filip <f-r-c@LAPTOP-BO73C4EV>
2021-02-24 09:21:24 +0100

    Add project files.

---
+++ DoorControllApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DoorControllApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
